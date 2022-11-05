420a7c6dcacb84c395a2e6c71270754d095189d3
20P31A04C1 <84897802+20P31A04C1@users.noreply.github.com>
2022-11-05 22:43:06 +0530

    import java.util.Scanner; class sample {     public static boolean ispalindrome(int n)     {     int r,sum=0,temp=n;     while(n>0)     {         r=n%10;         sum=sum*10+r;         n=n/10;     }     if(temp==sum)      return true;     else      return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i,c=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         a[i]=sc.nextInt();         for(i=0;i<n;i++)         {          if(ispalindrome(a[i]))          {          c++;          }         }         System.out.println(c);     } }

--- Count_palindromes.java
+++ Count_palindromes.java
@@ -3,31 +3,33 @@ class sample
 {
     public static boolean ispalindrome(int n)
     {
-        int sum=0,r,temp=n;
-        while(n>0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-        }
-        if(sum==temp)
-          return true;
-        else
-          return false;
+    int r,sum=0,temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum*10+r;
+        n=n/10;
+    }
+    if(temp==sum)
+     return true;
+    else
+     return false;
     }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,count=0;
+        int n,a[],i,c=0;
         n=sc.nextInt();
-        x=new int[n];
+        a=new int[n];
         for(i=0;i<n;i++)
-        x[i]=sc.nextInt();
+        a[i]=sc.nextInt();
         for(i=0;i<n;i++)
         {
-            if(ispalindrome(x[i]))
-            count++;
+         if(ispalindrome(a[i]))
+         {
+         c++;
+         }
         }
-        System.out.println(count);
+        System.out.println(c);
     }
 }
