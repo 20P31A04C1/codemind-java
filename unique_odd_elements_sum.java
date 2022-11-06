cbbad5bcd47dcd5733051c67e3732a4beb1a3c1e
20P31A04C1 <84897802+20P31A04C1@users.noreply.github.com>
2022-11-06 15:16:35 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,n,a[],s=0;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         a[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]%2!=0 && a[i]!=-999)             {                 for(j=0;j<n;j++)                 {                     if(a[i]==a[j] && i!=j)                     {                         a[j]=-999;                     }                 }                 s=s+a[i];
                }
            }
            System.out.print(s);
               
        }
    }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,n,a[],s=0;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        a[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]%2!=0 && a[i]!=-999)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(a[i]==a[j] && i!=j)
+                    {
+                        a[j]=-999;
+                    }
+                }
+                s=s+a[i];
+                
+            }
+        }
+        System.out.print(s);
+           
+    }
+}
