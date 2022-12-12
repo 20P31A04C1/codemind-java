239cad13cb91990a318908f4f73e70d99c0c801e
20P31A04C1 <84897802+20P31A04C1@users.noreply.github.com>
2022-12-12 11:42:12 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a[],n,i,k,c=0;
            n=sc.nextInt();
            a=new int[n];
            for(i=0;i<n;i++)
            {
                a[i]=sc.nextInt();
            }
            k=sc.nextInt();
            for(i=0;i<n;i=i+1)
            {
                if(a[i]==k)
                c=1;
            }
            if(c==1)
              System.out.println("True");
            else
              System.out.println("False");
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a[],n,i,k,c=0;
+         
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i=i+1)
+        {
+            if(a[i]==k)
+            c=1;
+        }
+        if(c==1)
+          System.out.println("True");
+        else
+          System.out.println("False");
+    }
+}
